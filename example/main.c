bd0ed17bb0cefe642cad98284f30e656ff2cd6a7
Saugat913 <kandelsaugat913@gmail.com>
2024-06-02 13:16:03 +0545

    feature:add the api for library

--- example/main.c
+++ example/main.c
@@ -1,14 +1,24 @@
 #include "cfger/config.h"
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
 
-    struct ArrayOfSection sections = parse_config_file("config.txt");
+    cfger_Config config = cfger_parse_config_file("config.txt");
 
-    for (size_t i = 0; i < sections.count; i++)
+    if (config == NULL)
     {
-        display_section(&sections.sections[i]);
+        printf("ERROR while opening file\n");
+        return 1;
     }
+
+    cfger_Section new_test_section = cfger_create_section("test", config);
+
+    cfger_set_str_value("key1", "value1", new_test_section);
+
+    cfger_display_config(config);
+
+    cfger_destroy_config(config);
 
     return 0;
 }
